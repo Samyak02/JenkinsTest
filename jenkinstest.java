e69572658191829e18ee4fa9d7815aae5d296fbe
Samyak02 <56731043+Samyak02@users.noreply.github.com>
2024-04-07 20:21:37 +0530

    Add files via upload

---
+++ jenkinstest.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class jenkinstest {
+
+	public static void main(String[] args)
+	{
+		System.out.println("**********************");
+		System.out.println("Hello Samyak");
+		System.out.println("**********************");
+		
+	}
+	
+	
+}
